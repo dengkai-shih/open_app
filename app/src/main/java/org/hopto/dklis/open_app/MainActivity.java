3a3296a726c7319335bda04b5587165fb3162e05
dengkai-shih <dengkai.shih@gmail.com>
2020-05-27 15:33:32 +0800

    import 'RobotActivityLibrary' + 'ZenboSDK'

--- app/src/main/java/org/hopto/dklis/open_app/MainActivity.java
+++ app/src/main/java/org/hopto/dklis/open_app/MainActivity.java
@@ -3,20 +3,120 @@ package org.hopto.dklis.open_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.asus.robotframework.API.RobotAPI;
+import com.asus.robotframework.API.RobotCallback;
+import com.asus.robotframework.API.RobotCmdState;
+import com.asus.robotframework.API.RobotCommand;
+import com.asus.robotframework.API.RobotErrorCode;
+import com.asus.robotframework.API.results.DetectPersonResult;
+import com.asus.robotframework.API.results.GesturePointResult;
+import com.robot.asus.robotactivity.RobotActivity;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class MainActivity extends RobotActivity {
 
     public Button my_button, sensor_button;
+    public static Context context;
+    // static robotAPI
+    public static RobotAPI x_robotAPI;
 
+    public static RobotCallback robotCallback = new RobotCallback() {
+        @Override
+        public void onResult(int cmd, int serial, RobotErrorCode err_code, Bundle result) {
+            super.onResult(cmd, serial, err_code, result);
+        }
+
+        @Override
+        public void onStateChange(int cmd, int serial, RobotErrorCode err_code, RobotCmdState state) {
+            super.onStateChange(cmd, serial, err_code, state);
+
+            String txt_out = "onStateChange:"
+                    + RobotCommand.getRobotCommand(cmd).name()
+                    + ", serial:" + serial + ", err_code:" + err_code
+                    + ", state:" + state.toString();
+
+            Toast.makeText(context, txt_out, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onDetectPersonResult(List<DetectPersonResult> resultList) {
+            super.onDetectPersonResult(resultList);
+
+            Log.d("RobotDevSample", "onDetectPersonResult: " + resultList.get(0).getBodyLoc().toString());
+
+            // use toast to show detected persons
+            String toast_result = "Detect Person";
+            Toast toast = Toast.makeText(context, toast_result, Toast.LENGTH_SHORT);
+            toast.show();
+
+        }
+
+        @Override
+        public void onGesturePoint(GesturePointResult result) {
+            super.onGesturePoint(result);
+        }
+
+        @Override
+        public void initComplete() {
+            super.initComplete();
+            x_robotAPI.vision.requestDetectPerson(1000);
+        }
+    };
+
+    public static RobotCallback.Listen robotListenCallback = new RobotCallback.Listen() {
+        @Override
+        public void onFinishRegister() {
+
+        }
+
+        @Override
+        public void onVoiceDetect(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onSpeakComplete(String s, String s1) {
+
+        }
+
+        @Override
+        public void onEventUserUtterance(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onResult(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onRetry(JSONObject jsonObject) {
+
+        }
+    };
+
+    public MainActivity() {
+        super(robotCallback, robotListenCallback);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+        x_robotAPI = this.robotAPI;
 
         my_button = (Button)findViewById(R.id.button);
         my_button.setOnClickListener(new Button.OnClickListener(){
@@ -56,4 +156,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // start detect person
+        x_robotAPI.vision.requestDetectPerson(1000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // stop detect person
+        x_robotAPI.vision.cancelDetectPerson();
+    }
+
 }
